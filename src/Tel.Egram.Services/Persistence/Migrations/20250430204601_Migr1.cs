d981d329bd77fa4d5c50c966d4b33c47a146824a
M-L-Ml <m.l.malinouski@gmail.com>
2025-04-30 22:48:14 +0200

    Made  > dotnet -d ef migrations add Migr1

---
+++ src/Tel.Egram.Services/Persistence/Migrations/20250430204601_Migr1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tel.Egram.Services.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migr1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "key_value",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "key_value",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
